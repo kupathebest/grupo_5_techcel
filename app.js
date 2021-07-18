3279e419820043f45cfeb259e3fe85470cf59cce
Ana1.zap <anarolon.ar@gmail.com>
2021-07-17 21:22:42 -0300

    creando extructura

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','index.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
+app.get('/detail',(req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
+
+app.listen(port,() => console.log('Servidor corriendo en http://localhost:' + port))
