93553ed32de34fe3f530ab153da242646af45394
melisadiaz18 <melisaadiaz.mad@gmail.com>
2021-08-05 21:19:20 -0300

    completando correcciones

--- sprint2/app.js
+++ sprint2/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { dirname } = require('path');
+/*const { dirname } = require('path');*/
 const app = express();
 const port = 3030;
 const path = require('path');
@@ -8,7 +8,10 @@ app.use(express.static('public'));
 
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','index.html')));
-app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views','login.html')));
+
+
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+
 app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
 app.get('/detail',(req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
