a82511e60a23941ac3ddf7d41e3dd9bd7e85d3c6
Ana1.zap <anarolon.ar@gmail.com>
2021-08-17 20:57:17 -0300

    agrego envios

--- sprint2/app.js
+++ sprint2/app.js
@@ -11,9 +11,9 @@ app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','index.htm
 
 
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-
 app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
 app.get('/detail',(req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
+app.get('/envios',(req,res) => res.sendFile(path.join(__dirname,'views', 'envios.html' )));
 
 app.listen(port,() => console.log('Servidor corriendo en http://localhost:' + port))
