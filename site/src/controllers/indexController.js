fb76877f2b40699420b1355046e6200d7d87b9b9
kupathebest <kupa.thebest@gmail.com>
2021-08-19 19:46:02 -0300

    agregando rutas index

---
+++ site/src/controllers/indexController.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+let celulares = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','celulares.json'),'utf-8'));
+
+module.exports = {
+    index : (req,res) => {
+        celulares = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','celulares.json'),'utf-8'));
+        return res.render('index',{
+            celulares,
+        })
+    },
+    envios : (req, res) => {
+        return res.render('envios')
+    },
+    contactanos : (req, res) => {
+        return res.render('contactanos')
+    },
+    comoComprar : (req, res) => {
+        return res.render('comoComprar')
+    }
+
+}
--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const {index, envios, contactanos, comoComprar, } = require('../controllers/indexController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index);
+router.get('/envios', envios)
+router.get('/contactanos', contactanos)
+router.get('/comoComprar', comoComprar)
 
 module.exports = router;
