6c3a439072e19f4347af0da2221cc0a278ee44e3
kupathebest <kupa.thebest@gmail.com>
2021-08-21 15:36:52 -0300

    carrousel de index sin condicionales

--- site/public/javascripts/carrousel.js
+++ site/public/javascripts/carrousel.js
@@ -31,3 +31,69 @@ new Glider(document.querySelector('.glider'), {
       }
     ]
   });
+  new Glider(document.querySelector('.glider1'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 425px
+        breakpoint: 428,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 768px
+        breakpoint: 771,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
+  new Glider(document.querySelector('.glider2'), {
+    // Mobile-first defaults
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 425px
+        breakpoint: 428,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 768px
+        breakpoint: 771,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
