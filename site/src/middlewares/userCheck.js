5102dc8a2336735df3e8f4348eb94d98914d136f
Gafala <gastonfablafuente@gmail.com>
2021-09-11 00:52:54 -0300

    corrigiendo middlewares

--- site/src/middlewares/userCheck.js
+++ site/src/middlewares/userCheck.js
@@ -1,7 +1,9 @@
 module.exports = (req,res,next) => {
-    if(req.session.userLogin && req.session.userLogin.rol === "user" || req.session.userLogin.rol === "admin"){
+    if(req.session.userLogin){
+       if(req.session.userLogin.rol === "user" || req.session.userLogin.rol === "admin"){
         next()
+       }
     }else{
-        res.redirect('login')
+        res.redirect('/')
     }
 }
