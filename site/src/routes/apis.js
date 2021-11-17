0c637e05253920ae30126399e58664d32dfe8510
kupathebest <kupa.thebest@gmail.com>
2021-11-17 18:31:37 -0300

    ordenando rutas apis

--- site/src/routes/apis.js
+++ site/src/routes/apis.js
@@ -1,17 +1,15 @@
 var express = require('express');
-const {getFilterColour, getAllProducts, getFilterCategory, getFilterBrand } = require('../controllers/apis/productsController');
 var router = express.Router();
+const { getMails, verifyPassword } = require('../controllers/apis/usersController')
+const { addCategory, addColour } = require('../controllers/apis/adminController')
+const {getFilterColour, getAllProducts, getFilterCategory, getFilterBrand } = require('../controllers/apis/productsController');
 
-/* GET apis page. */
+
+/* /apis */
 router.get('/get-colours-filter', getFilterColour);
 router.get('/get-all-products', getAllProducts );
 router.get('/get-categories-filter',getFilterCategory);
 router.get('/get-brand-filter', getFilterBrand);
-var router = express.Router();
-const { getMails, verifyPassword } = require('../controllers/apis/usersController')
-const { addCategory, addColour } = require('../controllers/apis/adminController')
-
-/* /apis */
 router.get('/get-emails', getMails);
 router.post('/verify-password',verifyPassword)
 router.get('/add-category', addCategory)
